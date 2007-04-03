1386b530cea7332f0f887be4eb41dbf3530ab4e0
huangnankun <huangnankun@18014e2e-a62b-0410-9710-9d70511b414f>
2007-04-03 15:43:30 +0000

    Removed file/folder

--- trafsim/trunk/E180 Serialization/src/com/e180/vo/IJsonSerializable.java
+++
@@ -1,9 +0,0 @@
-package com.e180.vo;
-
-public interface IJsonSerializable {
-	// return the deserialized object
-	public Object unSerialize(String data);
-	
-	// serialize the object itself now
-	public String Serialize();
-}
